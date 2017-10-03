432c68f91ef794c398806b1f9673accb586676ae
crocodilepi <dinhvanfr@gmail.com>
2017-10-03 16:26:16 +0200

    mis a jour les doc

--- src/jmetis/MetisOptions.java
+++ src/jmetis/MetisOptions.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jmetis;
 
 import java.util.Arrays;
 
 /**
  * to manage options of MetisAPI
- *
  * @author dinhvan
  */
 public class MetisOptions {
